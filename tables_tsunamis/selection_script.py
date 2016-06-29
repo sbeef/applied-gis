f6cb056dd8d6f10cc08f5f4b8145f4b1cf9f1ed6
Joe Martin <jmartin@oberlin.edu>
2016-06-29 13:22:06 -0500

    added docstrings, soem comments, renamed, other tidying

--- tables_tsunamis/selection_script.py
+++ tables_tsunamis/selection_script.py
@@ -2,70 +2,74 @@ import arcpy
 arcpy.CheckOutExtension('Spatial')
 arcpy.env.overwriteOutput=True
 
+BELOW_GRIDCODE = 0
+ABOVE_GRIDCODE = 1
+ELEVATION_LAYER = 'elevation'
+POPULATION_LAYER = 'population'
+
 #reclassify
-def create_remap_table(break_value, raster_file):
+def create_remap_range(break_value, raster_file):
+    """Creates a remap range used to reclassify a raster"""
 		raster = arcpy.Raster(raster_file)
-		below = [raster.minimum, break_value, 0]
-		above = [break_value, raster.maximum, 1]
+		below = [raster.minimum, break_value, BELOW_GRIDCODE]
+		above = [break_value, raster.maximum, ABOVE_GRIDCODE]
 		return arcpy.sa.RemapRange([below, above])
 
-def new_sealevel_raster(break_value, raster_file, reclass_field="VALUE"):
-	remap_table = create_remap_table(break_value, raster_file)
+def new_elevation_raster(break_value, raster_file, reclass_field="VALUE"):
+  """creates a reclassified elevation raster"""
+	remap_table = create_remap_range(break_value, raster_file)
 	return arcpy.sa.Reclassify(raster_file, reclass_field, remap_table)
 
-def create_sealevel_polygon(break_value, raster_file, output_polygon, reclass_field="VALUE"):
-	new_raster = new_sealevel_raster(break_value, raster_file, reclass_field)
+def create_elevation_polygon(break_value, raster_file, output_polygon, 
+                             reclass_field="VALUE"):
+  """creates a polygon file with areas classified as below or 
+  above a given elevation
+
+  Keyword arguments:
+  break_value    -- the elevation to determine above and below
+  raster_file    -- the digital elevation model
+  output_polygon -- the file to store the elevation polygon
+  reclass_field  -- the field in the elevation raster with the elevation value (
+                    default 'VALUE')
+  """
+	new_raster = new_elevation_raster(break_value, raster_file, reclass_field)
 	arcpy.RasterToPolygon_conversion(new_raster, output_polygon)
 	return output_polygon
 
-#raster to polygon
-def get_affected_blocks(block_polygons, sea_polygons, selection_type):
-	block_layer = "in_memory/block_layer"
-	sea_layer = "in_memory/sea_layer"
-	print "sea polygons:%s" % sea_polygons
-	print arcpy.Exists(sea_polygons)
-	#arcpy.MakeFeatureLayer_management(block_polygons, block_layer)
-	#arcpy.MakeFeatureLayer_management(sea_polygons, sea_layer)
-	sea_layer = sea_polygons
-	block_layer = block_polygons
-	print arcpy.Exists(block_layer)
-	print arcpy.Exists(sea_layer)
-	print "in features:%s\nselect features:%s\noverlap type:%s" % (block_layer, sea_layer, selection_type)
-	arcpy.SelectLayerByLocation_management(block_layer, overlap_type=selection_type, select_features=sea_layer)
-	#arcpy.CopyFeatures_management(block_layer)
-	return block_layer
-#select
-
-#sum
 def sum_field(feature, field):
+  """sums all of a given numberical field of a given feature"""
 	cursor = arcpy.da.SearchCursor(feature, [field])
 	val = 0 
 	for row in cursor:
 		val = val + row[0]
 	return val
 
-def add_field(row, field, val):
-	val += row[field]
-	return val
+def sum_below_elevation(population_feature, elevation_raster, elevation, 
+                       spatial_relationshp, population_field)
+	"""Return the number of people below a certain elevation.
 
-def make_field_adder(field):
-	adder = lambda row, val: add_field(row, field, val) #look up lambda notation
-	return adder
-
-def sum_feature(feature, field):
-	adder = make_field_adder(field)
-	sum = apply_to_feature(feature, adder)
-	return sum
-
-def sum_below_sealevel(population_feature, elevation_raster, sealevel, selection_type, population_field):
-	sealevel_polygon = "in_memory/sealevel" #"Y:/Documents/oberlin gis/tables tsunami/sealevel_test.shp" #"in_memory/sealevel"
-	sealevel_polygon = create_sealevel_polygon(sealevel, elevation_raster, sealevel_polygon)
-	arcpy.MakeFeatureLayer_management(sealevel_polygon, "sealevel")
-	arcpy.MakeFeatureLayer_management(population_feature, "blocks")
-	#select sewalevel stuff
-	arcpy.SelectLayerByAttribute_management("sealevel", "NEW_SELECTION", " \"GRIDCODE\" = 0")
-#	arcpy.SaveToLayerFile_management("sealevel", "sealevel_polygon", "RELATIVE")
-	selected_blocklayer = get_affected_blocks("blocks", "sealevel", selection_type)
-	#selected_blocklayer = get_affected_blocks(population_feature, sealevel_polygon, selection_type)
+	Keyword arguments:
+	population_feature	-- a shapefile containing population information
+	elevation_raster		-- a digitial elevation for the region
+	elevation 				  -- the elevation to use in the calculation
+	spatial_relationshp	-- the spatial relationship or list of spatial 
+                         relationshps used to calculate overlap by ArcGIS
+	population_field		-- the field in the population file with population data
+  """
+  # create the elevation polygon 
+  # (polygons classify area as below or above elevation)
+	elevation_polygon = "in_memory/%s" % ELEVATION_LAYER
+	elevation_polygon = create_elevation_polygon(elevation, elevation_raster, 
+                                               elevation_polygon)
+  # create layers for elevation and population
+	arcpy.MakeFeatureLayer_management(elevation_polygon, ELEVATION_LAYER)
+	arcpy.MakeFeatureLayer_management(population_feature, POPULATION LAYER)
+  # select polygons below a certain elevation
+  elevation_query = '"GRIDCODE" = %s' % BELOW_GRIDCODE
+	arcpy.SelectLayerByAttribute_management(ELEVATION_LAYER, 'NEW_SELECTION', 
+                                          elevation_query)
+  arcpy.SelectLayerByLocation_management(POPULATION_LAYER, 
+                                         overlap_type=spatial_relationshp, 
+                                         select_features=ELEVATION_LAYER)
 	sum = sum_field(selected_blocklayer, population_field)
 	return sum
